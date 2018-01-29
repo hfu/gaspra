3fb278ce89be842121e0a82fe941cfadbb3ed996
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-01-29 20:01:31 +0000

    update

--- gaspra.js
+++ gaspra.js
@@ -1,24 +1,23 @@
 const fs = require('fs')
 //const matrix = [2, 3, 1]
 const matrix = [6, 55, 25]
-//let dict = {}
 const lut = {
-  'Cstline': {layer: 'cstline', minzoom: 10, maxzoom: 14},
-  'Cntr': {layer: 'cntr', minzoom: 14, maxzoom: 14},
-  'RdEdg': {layer: 'rdedg', minzoom: 14, maxzoom: 14},
-  'ElevPt': {layer: 'elevpt', minzoom: 14, maxzoom: 14},
-  'BldL': {layer: 'bldl', minzoom: 14, maxzoom: 14},
-  'GCP': {layer: 'gcp', minzoom: 14, maxzoom: 14},
-  'WL': {layer: 'wl', minzoom: 13, maxzoom: 14},
-  'AdmPt': {layer: 'admpt', minzoom: 14, maxzoom: 14},
-  'RdComPt': {layer: 'rdcompt', minzoom: 14, maxzoom: 14},
-  'CommBdry': {layer: 'commbdry', minzoom: 13, maxzoom: 14},
-  'AdmBdry': {layer: 'admbdry', minzoom: 12, maxzoom: 14},
-  'WStrL': {layer: 'wstrl', minzoom: 13, maxzoom: 14},
-  'CommPt': {layer: 'commpt', minzoom: 14, maxzoom: 14},
-  'RailCL': {layer: 'railcl', minzoom: 12, maxzoom: 14},
-  'SBBdry': {layer: 'sbbdry', minzoom: 13, maxzoom: 14},
-  'SBAPt': {layer: 'sbapt', minzoom: 13, maxzoom: 14}
+  'Cstline': {layer: 'cstline', minzoom: 10, maxzoom: 15},
+  'Cntr': {layer: 'cntr', minzoom: 14, maxzoom: 15},
+  'RdEdg': {layer: 'rdedg', minzoom: 14, maxzoom: 15},
+  'ElevPt': {layer: 'elevpt', minzoom: 14, maxzoom: 15},
+  'BldL': {layer: 'bldl', minzoom: 15, maxzoom: 15},
+  'GCP': {layer: 'gcp', minzoom: 15, maxzoom: 15},
+  'WL': {layer: 'wl', minzoom: 14, maxzoom: 15},
+  'AdmPt': {layer: 'admpt', minzoom: 15, maxzoom: 15},
+  'RdCompt': {layer: 'rdcompt', minzoom: 15, maxzoom: 15},
+  'CommBdry': {layer: 'commbdry', minzoom: 14, maxzoom: 15},
+  'AdmBdry': {layer: 'admbdry', minzoom: 12, maxzoom: 15},
+  'WStrL': {layer: 'wstrl', minzoom: 14, maxzoom: 15},
+  'CommPt': {layer: 'commpt', minzoom: 14, maxzoom: 15},
+  'RailCL': {layer: 'railcl', minzoom: 12, maxzoom: 15},
+  'SBBdry': {layer: 'sbbdry', minzoom: 14, maxzoom: 15},
+  'SBAPt': {layer: 'sbapt', minzoom: 14, maxzoom: 15}
 }
 
 function jumpInto(matrix) {
@@ -31,12 +30,20 @@ function jumpInto(matrix) {
       if(!fs.existsSync(path)) continue
       const r = JSON.parse(fs.readFileSync(path, 'utf-8')).features
       for(let f of r) {
-        //let c = dict[f.properties.class]
-        //c = c ? c + 1 : 1
-        //dict[f.properties.class] = c
+	delete f.properties['lfSpanFr']
+	delete f.properties['lfSpanTo']
+	delete f.properties['devDate']
+	delete f.properties['orgMDId']
 	f.tippecanoe = lut[f.properties.class]
+	//delete f.properties['class']
+	delete f.properties['fid']
+	delete f.properties['type']
+	delete f.properties['orgGILvl']
+	if(f.properties['vis'] === '') delete f.properties['vis']
+	if(f.properties['name'] === '') delete f.properties['name']
 	console.log(JSON.stringify(f))
       }
+      if(y % 5000 === 0 && global.gc) global.gc()
     }
   }
 }
